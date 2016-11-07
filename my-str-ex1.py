e30e27d351383c75104a65edad8788f0bb6484a9
Craig McBean, Sr <cmcbeans@its.jnj.com>
2016-11-07 09:11:40 -0800

    my first python script

---
+++ my-str-ex1.py
@@ -0,0 +1,11 @@
+a_str1 = "Craig McBean"
+a_str2 = "Sheree-Annm Lewis-McBean"
+a_str3 = 'Sheyenne Lewis'
+a_str4 = raw_input("Enter fourth Name: ")
+
+print "{:>30}".format(a_str1)
+print "{:>30}".format(a_str2)
+print "{:>30}".format(a_str3)
+print "{:>30}".format(a_str4)
+
+
