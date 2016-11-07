cf56a58846ffc4bdc43765993a51f772709ebea6
Craig McBean, Sr <cmcbeans@its.jnj.com>
2016-11-07 11:35:20 -0800

    Ok Here are my python scripts for handeling files

---
+++ my-files-ex2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+
+f = open("my_new_file.txt", "w")
+f.write('whatever2\n')
+f.close()
+
---
+++ my-files-ex3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+
+with open("my_new_file.txt", "a") as f:
+    f.write('something else\n')
+
+
